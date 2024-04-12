01c772cf26400d3aa15e57361f8e19917cdb6774
Bùi Anh Tú <tubuianh2017@gmail.com>
2024-04-12 17:54:49 +0700

    update dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="navbar text-center">
+            <div class="row">
+                <ul>
+                    <li><a href="#" class="active">Home</a></li>
+                    <li>|</li>
+                    <li><a href="#">Đăng nhập</a></li>
+                    <li>|</li>
+                    <li><a href="#">Đăng ký</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
